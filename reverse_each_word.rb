c3ed0011abb74c907d284308d7a89f62c559864c
tgray017 <tgray017@gmail.com>
2018-09-22 14:17:00 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,9 @@
-def reverse_each_word(sentence)
-  array = sentence.split(" ")
-  reversed_array = []
-  array.each {|word| reversed_array << word.reverse}
-  reversed_array.join(" ")
-end
+#def reverse_each_word(sentence)
+#  array = sentence.split(" ")
+#  reversed_array = []
+#  array.each {|word| reversed_array << word.reverse}
+#  reversed_array.join(" ")
+#end
 
 
 def reverse_each_word(sentence)
